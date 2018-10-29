6735d182cb32e6efaaa6bf4a161ef34b2c2956bd
Joboman555 <Jonahspear512@gmail.com>
2018-10-29 15:34:55 -0400

    Actually parse columns well into matrix and check that the columns are the right size

--- parse_SEMS.m
+++ parse_SEMS.m
@@ -1,5 +1,18 @@
 fname = 'SEMS_140_RESULTS_181015_134310.dat';
 fid = fopen(fname,'r');
-datacell = textscan(fid, '%s', 'CommentStyle','#', 'delimiter', '\n');
+% Read in file as a 1xN vector
+datacell = textscan(fid, '%f', 'CommentStyle','#', 'Delimiter', ': \t' );
 fclose(fid);
-A.data = datacell{1};
+
+data = datacell{1};
+
+% There are 71 columns in the data file. However, we split each of the 
+% two date columns into 3 columns, which gives us 75.  
+NUM_COLS = 75;
+disp(['Scanned ', num2str(size(data,1) / NUM_COLS), ' rows of data.']);
+
+if mod(size(data, 1), NUM_COLS) ~= 0
+    disp('Error! Data has an unexpected number of columns. ')
+    disp('Please update NUM_COLS in parse_SEMS and try again.')
+end
+
