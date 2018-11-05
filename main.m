68e63e55091da8789a5c3c2dabe965c07494466f
Joboman555 <Jonahspear512@gmail.com>
2018-11-05 13:30:38 -0500

    Plot Average concentrations of Particles

--- main.m
+++ main.m
@@ -1,9 +1,12 @@
 format long g
 
 [ bin_concentrations, bin_diameters, start_times, end_times ] = parse_SEMS('SEMS_140_RESULTS_181015_134310.dat');
-split_timepoint = datetime('15-Oct-2018 14:20:20');
+split_timepoint = datetime('15-Oct-2018 20:20:20');
 [ bin_concentrations_before, start_times_before, end_times_before ] = get_entries_before( split_timepoint, bin_concentrations, start_times, end_times );
 [ bin_concentrations_after, start_times_after, end_times_after ] = get_entries_after( split_timepoint, bin_concentrations, start_times, end_times );
-size(bin_concentrations_before)
-size(bin_concentrations_after)
+
+plt_avg_concentrations( bin_diameters, bin_concentrations_before, bin_concentrations_after )
+
+% figure
+% plot(start_times, sum(bin_concentrations,2))
 
---
+++ plt_avg_concentrations.m
@@ -0,0 +1,7 @@
+function plt_avg_concentrations( bin_diameters, bin_concentrations_before, bin_concentrations_after )
+%plt_avg_concentrations plots average concentrations for each particle size
+    plot(bin_diameters, mean(bin_concentrations_before), bin_diameters, mean(bin_concentrations_after))
+    set(gca,'xscale','log')
+    xlim([min(bin_diameters), max(bin_diameters)])
+end
+
